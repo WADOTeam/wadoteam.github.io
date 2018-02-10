f2aa3aa128fec66c23036cd37f24becaa473888f
Alexandru Furculita <alex@furculita.net>
2018-02-10 15:04:01 +0200

    WIP

--- assets/js/login.js
+++ assets/js/login.js
@@ -5,6 +5,6 @@ $('.message a').click(function(){
 $("#login").click( function()
     {
         console.log("a");
-        window.location = "http://127.0.0.1/index/dashboard.html";
+        window.location = "../../dashboard.html";
     }
 );
