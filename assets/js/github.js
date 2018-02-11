db25dba0deb55dfd8e2649ba2ffa3657de581ccc
Alexandru Furculita <alex@furculita.net>
2018-02-11 23:43:04 +0200

    Add github login
    Signed-off-by: Alexandru Furculita <alex@furculita.net>

---
+++ assets/js/github.js
@@ -0,0 +1,74 @@
+(function ($, hello) {
+  hello.init({
+    github: 'ac472ac90516733d44d8'
+  }, {
+    redirect_uri: '../redirect.html'
+  })
+
+  var online = function (session) {
+    var currentTime = (new Date()).getTime() / 1000
+    return session && session.access_token && session.expires > currentTime
+  }
+
+  var showLoggedIn = function () {
+    $('.logged-in').show()
+    $('.not-logged-in').hide()
+  }
+  var hideLoggedIn = function () {
+    $('.logged-in').hide()
+    $('.not-logged-in').show()
+  }
+
+  var checkButton = function () {
+    var github = hello('github').getAuthResponse()
+    if (online(github)) {
+      showLoggedIn()
+    } else {
+      hideLoggedIn()
+    }
+  }
+
+  checkButton()
+
+  window.loginWithGithub = function () {
+    var github = hello('github').getAuthResponse()
+    if (online(github) != null) {
+      hello('github')
+        .logout({options: {force: true}})
+        .then(function () {
+          hideLoggedIn()
+        }, function (reason) { console.log(reason) })
+    } else {
+      hello('github')
+        .login({scope: 'repo, user'})
+        .then(function () {
+          showLoggedIn()
+        }, function (reason) { console.log(reason) })
+    }
+  }
+
+  window.userRepos = function () {
+    var github = hello('github')
+
+    github.api('/user/repos')
+      .then(function (r) {
+        var dataSet = []
+        $.each(r.data, function (key, item) {
+          console.log(item)
+          dataSet.push([item.full_name, item.language])
+        })
+
+        $('#repositories').DataTable({
+          data: dataSet,
+          'ordering': false,
+          'searching': false,
+          "pagingType": "full_numbers",
+          responsive: true,
+          columns: [
+            {title: 'Name'},
+            {title: 'Language'}
+          ]
+        })
+      })
+  }
+})(jQuery, hello, window)
